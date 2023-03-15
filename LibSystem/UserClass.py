9473d4aa71ecaf67615500206db1f6eff3a53a85
jclevecodes <joshua.cleveland02@gmail.com>
2023-03-15 11:33:43 -0400

    user class update

--- LibSystem/UserClass.py
+++ LibSystem/UserClass.py
@@ -3,27 +3,14 @@ class User:
         self.name = userName 
         self.id = userID
 
-
+    def __str__(self):
+        return f"Name: {self.name} ID: {self.id}"
+    
     def getName(self):
         return (self.name)
     
-    def setName(self, userName):
-        self.name = userName
-    
     def getID(self):
         return (self.id)
-    
-    def setID(self, userID):
-        self.id = userID
-
-#creating an object of the class
-random = User('John Doe', '243340')
-
-#print the call of the instance method using the object
-# print("------------")
-# print(random.getName())
-# print(random.getID())
-
 
 
 
